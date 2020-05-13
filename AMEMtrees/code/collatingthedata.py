73427bf9e388db5d76476a78691b27adb09e9f43
PetraGuy <petra.guy.uk@gmail.com>
2020-05-13 09:59:43 +0100

    wrangling

--- AMEMtrees/code/collatingthedata.py
+++ AMEMtrees/code/collatingthedata.py
@@ -4,19 +4,22 @@ Created on Tue May 12 16:03:25 2020
 Sort out ground flora in herbs and shrub dfs
 @author: petra
 """
+import pandas as pd
 
-#take the herbs and woody shrubs out of the ground flora
+#get the data files
 floralist = pd.read_csv('../data/grndspeciesfullinfoWS.csv')
 groundflora = pd.read_csv('../data/GroundFlora.csv')
+trees = pd.read_csv('../data/Trees.csv')
 
+
+#take the herbs and woody shrubs out of the ground flora
 herbs = floralist.loc[floralist['woodiness'] == 'h']
 shrubs = floralist.loc[floralist['woodiness'].isin (['ws','wt','sw'])]
+#these two lines give error message, but they do change the appearance of BRC column to int
+#the sci notation seem sto cause problems with joins
 herbs['BRC'] = herbs['BRC'].astype(int)
 shrubs['BRC'] = shrubs['BRC'].astype(int)
 
-
-#extract herbs and shrubs from ground flora so that shrubs are not included as
-#part of richness/diversity calculations
 
 #grnd flora is a bit of a mess, duplicates in BRC etc - easier to separate 
 #years 1 and 2
@@ -24,12 +27,11 @@ flora1 = groundflora.loc[:,['Site', 'Plot', 'Nest', 'CoverYr1','AmalgamsYr1']].d
 flora2 = groundflora.loc[:,['Site', 'Plot', 'Nest', 'CoverYr2','AmalgamsYr2']].drop_duplicates().dropna()
 
 #sci notation is an issue - BRC 92003 not recognised as same as 9.2003e4
-#try to eliminate
+#try to eliminate as above. 
 flora1['AmalgamsYr1'] = flora1['AmalgamsYr1'].astype(int)
 flora2['AmalgamsYr2'] = flora2['AmalgamsYr2'].astype(int)
 
-#now merge herbs/shrubs with flora1/2 to get herbfloray1, shrubflorayr1 etc
-
+#now merge herbs/shrubs with flora1/2 to get herbfloraYr1, shrubfloraYr1 etc
 #herbflora1 and 2 are all the herbs with site/plot etc, that were in Yr1 and 2 of Bunce
 herbflora1 = pd.merge(flora1,herbs, left_on = 'AmalgamsYr1', right_on = 'BRC',how = 'right')
 herbflora2= pd.merge(flora2,herbs, left_on = 'AmalgamsYr2', right_on = 'BRC',how = 'right')
@@ -39,7 +41,7 @@ shrubs1 = pd.merge(flora1,shrubs, left_on = 'AmalgamsYr1', right_on = 'BRC',how 
 shrubs2= pd.merge(flora2,shrubs, left_on = 'AmalgamsYr2', right_on = 'BRC',how = 'right')
 
 
-#################
+################# looking at some rows to check for errors############
 def check(df,site,plot,nest):
     rows = df.loc[(df['Site']==site)&(df['Plot']==plot)&(df['Nest']==nest)]
     print(rows)
@@ -53,4 +55,6 @@ check(flora2,1,2,3)
 
 check(shrubs2,2,3,4)
 check(flora2,2,3,4)
+###################################################################
+
 
